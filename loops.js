314bbe3969a161adadb1fe1fca08625faea7afe1
Rob Paik <robpaik@gmail.com>
2019-06-16 21:38:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,6 +19,15 @@ function whileLoop (number) {
   return "done"
 }
 
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop (num) {
-  
+ do {
+   console.log("I run once regardless")
+    } while (incrementVariable() < num)
 }
