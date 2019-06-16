d779ceff69b5165b6b8fb6cb2c70023227f5f33f
Rob Paik <robpaik@gmail.com>
2019-06-16 21:56:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -28,5 +28,5 @@ function incrementVariable() {
 function doWhileLoop (num) {
   do {
     console.log("I run once regardless")
-     } while (incrementVariable() < parseInt(num));
+     } while (incrementVariable() < num);
 }
